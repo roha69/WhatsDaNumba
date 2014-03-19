718c9839dc389592e282562393bd375d71d8a36f
roha <p1106732@IUTAINFOPC107.univ-lyon1.fr>
2014-03-19 11:30:47 +0100

    gen nb alea

--- src/whatsdanumba/Jeu.java
+++ src/whatsdanumba/Jeu.java
@@ -9,5 +9,22 @@ package whatsdanumba;
  * @author p1106732
  */
 public class Jeu {
+    private int borneInf;
+    private int borneSup;
     
+    private int difficulte;
+    private int nombre;
+    
+    public Jeu(){
+        difficulte = 0; //par défaut
+        borneInf = 0;
+        borneSup = 10;
+        
+        nombre = genNbreAleat(borneInf, borneSup);
+    }
+    
+    private static int genNbreAleat(int borneInf, int borneSup) {
+        return (int) ((borneSup - borneInf + 1) * Math.random() + borneInf);
+    }
+            
 }
