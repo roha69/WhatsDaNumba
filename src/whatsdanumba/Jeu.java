740fbf5fab16a0698c432814ba80d1b0c064851e
roha <p1106732@IUTAINFOPC107.univ-lyon1.fr>
2014-03-20 09:23:32 +0100

    commit 1

--- src/whatsdanumba/Jeu.java
+++ src/whatsdanumba/Jeu.java
@@ -23,8 +23,32 @@ public class Jeu {
         nombre = genNbreAleat(borneInf, borneSup);
     }
     
+    public void commencer(){
+        
+    }
+    
     private static int genNbreAleat(int borneInf, int borneSup) {
         return (int) ((borneSup - borneInf + 1) * Math.random() + borneInf);
+    }
+
+    public int getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+    
+    public void victoire(){
+        switch (difficulte){
+            case 0 :
+                borneSup+=(5/100*borneSup);
+            case 1 :
+                borneSup+=(10/100*borneSup);
+            case 2 : 
+                borneSup+=(15/100*borneSup);
+        }
+            
     }
             
 }
